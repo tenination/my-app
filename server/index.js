c69caee5142cbfc2a9b30def609179dfa5bcfff9
Tim Ninan <tim.ninan@gmail.com>
2017-12-29 21:23:50 -0600

    connected to mlab mongodb database

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+mongoose.connect('mongodb://test:test@ds135747.mlab.com:35747/my-app-db');
+
+const todoSchema = new mongoose.Schema({
+  item: String,
+});
+
+const Todo = mongoose.model('Todo', todoSchema);
+const itemOne = Todo({ item: 'buy flowers' }).save((err) => {
+  if (err) throw err;
+  console.log('item saved');
+});
 
 // Priority serve any static files
 app.use(express.static(path.resolve(__dirname, '../client/build')));
